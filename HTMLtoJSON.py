7191505e55f08d26f4b976e6358bf41769996fb6
juliandietz <julian.dietz@stud.uni-regensburg.de>
2018-06-22 23:13:10 +0200

    mögliches methodenlayout

--- HTMLtoJSON.py
+++ HTMLtoJSON.py
@@ -1,30 +1,93 @@
 from bs4 import BeautifulSoup
 import json
-from os import listdir
-from os.path import isfile, join
-import os
-from os.path import basename
+from os import listdir,path
+from os.path import isfile, join,basename
 
-htmlfilesdirectory='corpusRawHTML'
-outputdirectory='output'
-#open file
-for file in listdir(htmlfilesdirectory):
-    if isfile(join(htmlfilesdirectory, file)) and file.endswith('.html'):
-        file = open(join(htmlfilesdirectory, file))
-        print('parse file: '+ file.name)
 
-        htmlfile = BeautifulSoup(file, 'html.parser')
-        output = {}
-        htmlarticle=htmlfile.article
-        #print(htmlarticle)
 
-        title = htmlarticle.find("h1")
-        print(title.text)
+def parseHTML():
+    htmlfilesdirectory = 'corpusRawHTML'
+    outputdirectory = 'output'
+    # open file
+    for file in listdir(htmlfilesdirectory):
+        if isfile(join(htmlfilesdirectory, file)) and file.endswith('.html'):
+            file = open(join(htmlfilesdirectory, file))
+            print('parse file: ' + file.name)
 
-        output['title'] = title.text
+            htmlfile = BeautifulSoup(file, 'html.parser')
+            output = {}
+            htmlarticle = htmlfile.article
+            output['title'] = getTitel(htmlarticle)
 
-        #output['test'] = "Hello world"
-        name=os.path.splitext(basename(file.name))[0]
-        file = open(join(outputdirectory, name+'.json'), 'w')
-        json.dump(output, file)
-        file.close()
+            name = path.splitext(basename(file.name))[0]
+            file = open(join(outputdirectory, name + '.json'), 'w')
+            json.dump(output, file)
+            file.close()
+
+def readJsonFiles():
+    outputdirectory = 'output'
+    for file in listdir(outputdirectory):
+        # read json
+        print('read file: ' + file)
+        name = path.splitext(basename(file))[0]
+        file = open(join(outputdirectory, name + '.json'), 'r')
+        jsonfile = json.load(file)
+        print(jsonfile["title"])
+
+
+
+def getTitel(htmlArticle):
+    title=htmlArticle.find("h1").text
+    if title:
+        return title
+    else:
+        return 'no title found'
+
+def getallReferences(htmlArticle):
+    pass
+
+def getKeywords(htmlArticle):
+    pass
+
+def getAuthors(htmlArticle):
+    pass
+
+
+
+def getSelection(htmlArticle):
+    getInnerSection(htmlArticle)
+    SelectionImages(htmlArticle)
+    SelectionTables(htmlArticle)
+    SelectionFormeln(htmlArticle)
+
+    getSelectionTitle(htmlArticle)
+    getSelectionText(htmlArticle)
+    getNumSelectionReferences(htmlArticle)
+    pass
+
+def getSelectionTitle(htmlSelection):
+    pass
+
+def getSelectionText(htmlSelection):
+    pass
+
+
+def getNumSelectionReferences(htmlArticle):
+    pass
+
+def SelectionImages(htmlArticle):
+    pass
+
+def SelectionTables(htmlArticle):
+    pass
+
+def SelectionFormeln(htmlArticle):
+    pass
+
+def getInnerSection(htmlArticle):
+    #search if inner section --> getSelection(htmlArticle)
+    pass
+
+
+parseHTML()
+readJsonFiles()
