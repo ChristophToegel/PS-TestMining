cfc74e9bb2324f554b7cc03cf0702ca2de87ea51
juliandietz <julian.dietz@stud.uni-regensburg.de>
2018-06-22 20:24:03 +0200

    Script htmltojson initial commit

---
+++ HTMLtoJSON.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import json
+from os import listdir
+from os.path import isfile, join
+import os
+from os.path import basename
+
+htmlfilesdirectory='corpusRawHTML'
+outputdirectory='output'
+#open file
+for file in listdir(htmlfilesdirectory):
+    if isfile(join(htmlfilesdirectory, file)) and file.endswith('.html'):
+        file = open(join(htmlfilesdirectory, file))
+        print('parse file: '+ file.name)
+
+        htmlfile = BeautifulSoup(file, 'html.parser')
+        output = {}
+        htmlarticle=htmlfile.find("article")
+        print(htmlarticle)
+
+        title = htmlfile.find("title")
+        output['title'] = title.text
+
+        #output['test'] = "Hello world"
+        name=os.path.splitext(basename(file.name))[0]
+        file = open(join(outputdirectory, name+'.json'), 'w')
+        json.dump(output, file)
+        file.close()
