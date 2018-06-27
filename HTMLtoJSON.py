2baf0326d57c85de8d4fcb2cd2738a67f8910c31
juliandietz <julian.dietz@stud.uni-regensburg.de>
2018-06-27 20:16:15 +0200

    Konvertierung metadata,abstact angefangen

--- HTMLtoJSON.py
+++ HTMLtoJSON.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import json
 from os import listdir,path
 from os.path import isfile, join,basename
+import requests
 
 
 
@@ -18,6 +19,8 @@ def parseHTML():
             output = {}
             htmlarticle = htmlfile.article
             output['title'] = getTitel(htmlarticle)
+            output['metaData']= getMetadata(htmlfile)
+            output['abstract'] = getAbstract(htmlarticle)
 
             name = path.splitext(basename(file.name))[0]
             file = open(join(outputdirectory, name + '.json'), 'w')
@@ -46,8 +49,58 @@ def getTitel(htmlArticle):
 def getallReferences(htmlArticle):
     pass
 
-def getKeywords(htmlArticle):
-    pass
+def getKeywords(htmlfile):
+    print("keywords")
+    '''for div in htmlArticle.findAll('strong'):
+            if div.text and div.text == "Keywords:":
+                text=div.text
+                keywordsArray = div.parent.text.replace(text,"").strip()
+                break
+    return keywordsArray.split(";")'''
+    keywords=htmlfile.find("meta", {"name": "keywords"})['content']
+    print(keywords)
+    return keywords
+
+def getYear(htmlfile):
+    print('year')
+    year=htmlfile.find("meta", {"name": "citation_year"})['content']
+    print(year)
+    return int(year)
+
+def getAbstract(htmlArticle):
+    print("Abstract")
+    for div in htmlArticle.findAll('div'):
+        if div.find('h3'):
+            container=div.find('h3')
+            if container.text and container.text=="Abstract":
+                #print("Abstract found!!")
+                break
+    abstract=div.find('p').find('p')
+
+    print(len(abstract.findAll('br')))
+    if abstract.find('strong'):
+        list=[]
+        #list.append({'titel': 'no Titel', 'text': abstract.next_sibling})
+        for section in abstract.findAll('strong'):
+            #print(section)
+            #print(section.next_sibling)
+            list.append({'titel': section.text,'text':section.next_sibling})
+    elif abstract.find('br'):
+        list = []
+        #print(abstract.text.splitlines())
+        for section in abstract.text.splitlines():
+            #print(section)
+            list.append({'title':'no Titel','text':section})
+    else:
+        list = {'title': 'no Titel', 'text': abstract.text}
+    print(list)
+    return list
+
+def getMetadata(htmlfile):
+    output={}
+    output['keywords']=getKeywords(htmlfile)
+    output['yearOfArticle']=getYear(htmlfile)
+    return output
 
 def getAuthors(htmlArticle):
     pass
@@ -88,6 +141,5 @@ def getInnerSection(htmlArticle):
     #search if inner section --> getSelection(htmlArticle)
     pass
 
-
-parseHTML()
-readJsonFiles()
+#parseHTML()
+#readJsonFiles()
